c23973bc5ed3485390957a5734023cdbf0cf8bcd
javin <javincraig>
2019-03-10 10:55:57 -0700

    More adjustments Working.

--- networkinfo.py
+++ networkinfo.py
@@ -1,4 +1,7 @@
+# Create networks dictionary
 networks = {}
+
+# This is for one of the questions to kick it off.
 more = 'YES'
 
 # Yes/No input validation
@@ -7,55 +10,55 @@ yesans = ['Y', 'YE', 'YES']
 noans = ['N', 'NO']
 
 # Gather vlan/network information
-while more in yesans:
-    vlan = input('What is the vlan ID?: ')
-    while int(vlan) < 0 or int(vlan) > 4096:
+def network_info(networks):
+    more = 'YES'
+    while more in yesans:
         vlan = input('What is the vlan ID?: ')
-    vlan_name = input('What is the name for vlan ' + str(vlan) + '?: ')
-    while ' ' in vlan_name:
-        vlan_name = input(
-            'What is the name for vlan ' + str(vlan) + '?(this cannot contain spaces, use underscore if necessary): ')
-    vlan_network = input('What is the network for vlan ' + str(vlan) + '?: ')
-    vlan_mask = input('What is the network mask for vlan ' + str(vlan) + '?: ')
-    vlan_gateway = input('What is the default gateway for vlan ' + str(vlan) + '?: ')
-    networks[vlan] = vlan
-    networks[vlan] = {}
-    networks[vlan]['vlanid'] = vlan
-    networks[vlan]['vlanname'] = vlan_name
-    networks[vlan]['vlannetwork'] = vlan_network
-    networks[vlan]['vlanmask'] = vlan_mask
-    networks[vlan]['vlangateway'] = vlan_gateway
-    more = input('Are there any more VLANs(YES/NO)?: ').upper()
-    while more not in yesnoans:
+        vlan_name = input('What is the name for vlan ' + str(vlan) + '?: ')
+        while ' ' in vlan_name:
+            vlan_name = input(
+                'What is the name for vlan ' + str(
+                    vlan) + '?(this cannot contain spaces, use underscore if necessary): ')
+        vlan_network = input('What is the network for vlan ' + str(vlan) + '?: ')
+        vlan_mask = input('What is the network mask for vlan ' + str(vlan) + '?: ')
+        vlan_gateway = input('What is the default gateway for vlan ' + str(vlan) + '?: ')
+        networks[vlan] = vlan
+        networks[vlan] = {}
+        networks[vlan]['vlanid'] = vlan
+        networks[vlan]['vlanname'] = vlan_name
+        networks[vlan]['vlannetwork'] = vlan_network
+        networks[vlan]['vlanmask'] = vlan_mask
+        networks[vlan]['vlangateway'] = vlan_gateway
         more = input('Are there any more VLANs(YES/NO)?: ').upper()
+        while more not in yesnoans:
+            more = input('Are there any more VLANs(YES/NO)?: ').upper()
 
-# gather switch management information
+
+# Run the gathering of networks
+network_info(networks)
+
+# Get the Switch Information
+# Gather switch management VLAN
 managementvlan = input('What is the switch management vlan?: ')
-# validating that the vlan was gathered above
+# Validate that the management vlan was gathered above
 if managementvlan not in networks:
     print('Did you forget to add the management vlan!?')
-    vlan = input('What is the management vlan ID?: ')
-    while int(vlan) < 0 or int(vlan) > 4096:
-        vlan = input('What is the vlan ID? (1-4096): ')
-    vlan_name = input('What is the name for vlan ' + str(vlan) + '?: ')
-    vlan_network = input('What is the network for vlan ' + str(vlan) + '?: ')
-    vlan_mask = input('What is the network mask for vlan ' + str(vlan) + '?: ')
-    vlan_gateway = input('What is the default gateway for vlan ' + str(vlan) + '?: ')
-    networks[vlan] = vlan
-    networks[vlan] = {}
-    networks[vlan]['vlanid'] = vlan
-    networks[vlan]['vlanname'] = vlan_name
-    networks[vlan]['vlannetwork'] = vlan_network
-    networks[vlan]['vlanmask'] = vlan_mask
-    networks[vlan]['vlangateway'] = vlan_gateway
+    network_info(networks)
+
+# Get the rest of the information
 managementip = input('What is the management IP?: ')
 switchhostname = input('What is the hostname for the switch?: ')
 enable_secret = input('What is the enable secret password?[cisco]: ') or 'cisco'
 user = input('What username would you like to create?[cisco]: ') or 'cisco'
 user_password = input('What is the password?[cisco]: ') or 'cisco'
+
+# Get the ESXi Information
+esxi_switch_name = input('What is the virtual switch name? [LAN]: ') or 'LAN'
+vmnic = input('what is the virtual switch uplink? [vmnic1]: ').lower() or 'vmnic1'
+
+
+# Build ESXi Configuration
 def esxi_conf(networks):
-    esxi_switch_name = input('What is the virtual switch name? [LAN]: ') or 'LAN'
-    vmnic = input('what is the virtual switch uplink? [vmnic1]: ').lower() or 'vmnic1'
     print('')
     print('-------------------------ESXI CLI Cconfiguration---------------------------------')
     print('esxcli network vswitch standard add --vswitch-name=' + esxi_switch_name)
@@ -66,6 +69,8 @@ def esxi_conf(networks):
         print('esxcli network vswitch standard portgroup add --portgroup-name=' + networks[net]['vlanname'] + ' --vswitch-name=' + esxi_switch_name)
         print('esxcli network vswitch standard portgroup set -p ' + networks[net]['vlanname'] + ' --vlan-id ' + networks[net]['vlanid'])
 
+
+# Build Cisco Switch Configuration
 def cisco_switch_conf(networks):
     print('')
     print('Copy and paste the following on to your switch from the console port')
@@ -95,6 +100,7 @@ ssh version 2
     print('exit')
     print('ip default gateway ' + networks[managementvlan]['vlangateway'])
 
-# Trigger the above definition
-cisco_switch_conf(networks)
+
+# Trigger the Switch config and build
 esxi_conf(networks)
+cisco_switch_conf(networks)
